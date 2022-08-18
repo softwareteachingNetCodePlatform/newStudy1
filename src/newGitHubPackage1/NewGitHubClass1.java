442097743ad4571b43c8e50e22075325d1e48f2d
hasan <getdatapro@gmail.com>
2022-08-18 15:07:30 +0300

    newV2

--- src/newGitHubPackage1/NewGitHubClass1.java
+++ src/newGitHubPackage1/NewGitHubClass1.java
@@ -3,5 +3,6 @@ package newGitHubPackage1;
 public class NewGitHubClass1 {
     public static void main(String[] args) {
         System.out.println("GitHub Deneme");
+        System.out.println("GitHub Deneme-2");
     }
 }

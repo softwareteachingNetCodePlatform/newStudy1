26133a0a9edf79538e6cde14ee688f9ea8c2896b
hasan <getdatapro@gmail.com>
2022-08-18 19:43:29 +0300

    B1

--- src/newGitHubPackage1/NewGitHubClass1.java
+++ src/newGitHubPackage1/NewGitHubClass1.java
@@ -4,5 +4,6 @@ public class NewGitHubClass1 {
     public static void main(String[] args) {
         System.out.println("GitHub Deneme");
         System.out.println("GitHub Deneme-2");
+        //merge yapıyoruz,hasandayız...
     }
 }

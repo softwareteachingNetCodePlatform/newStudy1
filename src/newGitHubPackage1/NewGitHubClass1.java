fbec0f15697107359f8f439e28ce8e6037623339
hasan <getdatapro@gmail.com>
2022-08-18 19:52:52 +0300

    mergeV1

--- src/newGitHubPackage1/NewGitHubClass1.java
+++ src/newGitHubPackage1/NewGitHubClass1.java
@@ -5,5 +5,8 @@ public class NewGitHubClass1 {
         System.out.println("GitHub Deneme");
         System.out.println("GitHub Deneme-2");
         //merge yapıyoruz,hasandayız...
+
+        // ikinci merge deneme
+
     }
 }

f9ccce5fc2f84792a11e80c8842770b8d2c149d3
softwareteachingNetCodePlatform <111507000+softwareteachingNetCodePlatform@users.noreply.github.com>
2022-08-19 18:58:56 +0300

    Update NewGitHubClass1.java

--- src/newGitHubPackage1/NewGitHubClass1.java
+++ src/newGitHubPackage1/NewGitHubClass1.java
@@ -6,7 +6,7 @@ public class NewGitHubClass1 {
         System.out.println("GitHub Deneme-2");
         System.out.println("GitHub Deneme-3");
         System.out.println("GitHub dan Gelen Satır")
-         // gitHub dan giden yeni-19.08.2022
+         // gitHub dan giden yeni-19.08.2022.
 
     }
 }
